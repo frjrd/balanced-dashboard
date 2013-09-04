f32b97c33328a97fe4a667ff8b2c16317e192531
kody Peterson <headwebmonkey@gmail.com>
2013-09-03 20:35:48 -0400

    Only showing refund button for non-refund/partially refunded debit

--- app/models/debit.js
+++ app/models/debit.js
@@ -1,11 +1,22 @@
+require('app/models/transaction');
+
 Balanced.Debit = Balanced.Transaction.extend({
     source: Balanced.Model.belongsTo('source', 'Balanced.FundingInstrument'),
     hold: Balanced.Model.belongsTo('hold', 'Balanced.Hold'),
     refunds: Balanced.Model.hasMany('refunds', 'Balanced.Refund'),
 
     refunded: function() {
-        return this.get('refunds');
-    }.property('refunds'),
+        var refunds = this.get('refunds.content');
+        var refundAmount = _.filter(refunds, function(refund) {
+            return refund.get('id');
+        });
+        var amount = 0;
+        for(var i = 0; i < refundAmount.length; i++){
+            amount += refundAmount[i].amount;
+        }
+
+        return this.get('status') !== 'failed' && this.get('refunds.isLoaded') && this.get('refunds.content') && this.get('refunds.content').length !== 0 && this.get('amount') === amount;
+    }.property('status', 'refunds.isLoaded', 'id', 'amount'),
 
     type_name: function () {
         return "Debit";
